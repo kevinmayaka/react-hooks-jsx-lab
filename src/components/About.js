028d6787c465959fcb952ee4d71767a41de6d5ec
kevin mayaka <kevinabuga3@gmail.com>
2024-04-24 11:12:04 +0300

    initial commit

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,14 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
+  <div id="about">
+    <h2>About Me</h2>
+    <p>Any content of my choice</p>
+    <img
+      src={image};
+      alt="I made this"
+    />
+  </div>
   return <div>About</div>;
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,9 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
+  <div id="home">
+    <h1 style={{ color: "firebrick" }}> Your {name} is a Web Developer from {city}</h1>
+  </div>
   return <div>Home</div>;
 }
 
